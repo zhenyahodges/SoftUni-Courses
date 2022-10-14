1b7f1a4c43617a8bf0166b4257944a7666653539
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2022-10-14 17:10:28 +0300

    added lecture demo files

---
+++ JS_BackEnd/01_Intro_NodeJS/calc.js
@@ -0,0 +1,11 @@
+function calc(a, b) {
+  return a + b;
+}
+
+//default
+// module.exports=calc
+
+//named
+exports.calc = calc;
+
+exports.multiply = (a, b) => a * b;
---
+++ JS_BackEnd/01_Intro_NodeJS/calcApp.js
@@ -0,0 +1,13 @@
+//default import
+// const calc = require("./calc.js");
+
+//named import
+const { calc,multiply } = require("./calc.js");
+
+function init() {
+  let result = calc(2, 3);
+  console.log(result);
+  console.log(multiply(4,2));
+}
+
+init();
