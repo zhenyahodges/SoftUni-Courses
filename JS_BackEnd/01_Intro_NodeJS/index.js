e232eedb1e09ad861b84c5cc41e5eb5bbb1be1f1
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2022-10-16 18:19:48 +0300

    test features

--- JS_BackEnd/01_Intro_NodeJS/index.js
+++ JS_BackEnd/01_Intro_NodeJS/index.js
@@ -1,8 +1,42 @@
-const http= require('http');
+const http = require("http");
 
-const server=http.createServer((re,res)=>{
-    console.log('Req received');
-})
+const homePage = `<!DOCTYPE html>
+<html lang="en">
+<body>
+<a href="/about">About</a>
+    <h1>Hello, world!</h1>
+</body>
+</html>`;
 
-server.listen(3000)
+const aboutPage = `<!DOCTYPE html>
+<html lang="en">
+<body>
+<a href="/">Home</a>
+    <h1>About</h1>
+</body>
+</html>`;
 
+const server = http.createServer((req, res) => {
+  console.log("Req received");
+  // console.log(req)
+  console.log(req.method);
+  console.log(req.headers);
+  console.log(req.url);
+
+  if (req.url == "/") {
+    // res.writeHead(200, ['Content-Type', 'text/plain'])
+    // res.write('<h1>');
+    // res.write('Hello, world!');
+    // res.write('</h1>');
+    res.write(homePage);
+    res.end();
+  } else if (req.url == "/about") {
+    res.write(aboutPage);
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
+});
+
+server.listen(3000);
