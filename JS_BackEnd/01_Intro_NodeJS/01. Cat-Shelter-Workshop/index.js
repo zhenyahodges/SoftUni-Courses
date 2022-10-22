fabaeb37a89bdf8d7a6d5f7d45e24d6ef52f5417
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2022-10-22 21:49:44 +0100

    setup server

--- JS_BackEnd/01_Intro_NodeJS/01. Cat-Shelter-Workshop/index.js
+++ JS_BackEnd/01_Intro_NodeJS/01. Cat-Shelter-Workshop/index.js
@@ -1,2 +1,11 @@
 const http= require('http')
+const port=3000
 
+http.createServer((req,res)=>{
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    })
+
+    res.write('Hello, world')
+    res.end()
+}).listen(port)
