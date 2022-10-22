71ab356826527d35329245e952ffc73fc212d504
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2022-10-22 08:34:36 +0100

    added single html

--- JS_BackEnd/01_Intro_NodeJS/index.js
+++ JS_BackEnd/01_Intro_NodeJS/index.js
@@ -1,66 +1,90 @@
 const http = require("http");
 
-const homePage = `<!DOCTYPE html>
-<html lang="en">
-<body>
-<a href="/about">About</a>
-    <h1>Hello, world!</h1>
-</body>
-</html>`;
+// const homePage = `<!DOCTYPE html>
+// <html lang="en">
+// <body>
+// <a href="/about">About</a>
+//     <h1>Welcome to my site!</h1>
+// </body>
+// </html>`;
 
-const aboutPage = `<!DOCTYPE html>
-<html lang="en">
-<body>
+// const aboutPage = `<!DOCTYPE html>
+// <html lang="en">
+// <body>
+// <a href="/">Home</a>
+//     <h1>About us...</h1>
+// </body>
+// </html>`;
+
+const homePage = `
+<a href="/about">About</a>
+    <h1>Welcome to my site!</h1>`;
+
+const aboutPage = `
 <a href="/">Home</a>
-    <h1>About</h1>
-</body>
-</html>`;
+    <h1>About us...</h1>`;
+
+    const defaultPage = `
+    <h1>404 Not Found</h1>`
 
 const server = http.createServer((req, res) => {
   console.log("Req received");
   // console.log(req)
-  
-  console.log(req.method);
-  console.log(req.headers);
-  console.log(req.url);
+
+  // console.log(req.method);
+  // console.log(req.headers);
+  // console.log(req.url);
+
+  console.log(">>>", req.method, req.url);
 
   // const url=new URL(req.url, 'http://localhost:3000')
-  const url=new URL(req.url, `http://${req.headers.host}`)
- 
+  const url = new URL(req.url, `http://${req.headers.host}`);
+
   console.log(url);
 
+  //   if (req.url == "/") {
+  //     // res.writeHead(200, ['Content-Type', 'text/plain'])
+  //     // res.write('<h1>');
+  //     // res.write('Hello, world!');
+  //     // res.write('</h1>');
+  //     res.write(homePage);
+  //     res.end();
+  //   } else if (req.url == "/about") {
+  //     res.write(aboutPage);
+  //     res.end();
+  //   } else {
+  //     res.statusCode = 404;
+  //     res.end();
+  //   }
+  // });
 
-//   if (req.url == "/") {
-//     // res.writeHead(200, ['Content-Type', 'text/plain'])
-//     // res.write('<h1>');
-//     // res.write('Hello, world!');
-//     // res.write('</h1>');
-//     res.write(homePage);
-//     res.end();
-//   } else if (req.url == "/about") {
-//     res.write(aboutPage);
-//     res.end();
-//   } else {
-//     res.statusCode = 404;
-//     res.end();
-//   }
-// });
+  if (url.pathname == "/") {
+    // res.writeHead(200, ['Content-Type', 'text/plain'])
+    // res.write('<h1>');
+    // res.write('Hello, world!');
+    // res.write('</h1>');
 
-
-if (url.pathname == "/") {
-  // res.writeHead(200, ['Content-Type', 'text/plain'])
-  // res.write('<h1>');
-  // res.write('Hello, world!');
-  // res.write('</h1>');
-  res.write(homePage);
-  res.end();
-} else if (url.pathname == "/about") {
-  res.write(aboutPage);
-  res.end();
-} else {
-  res.statusCode = 404;
-  res.end();
-}
+    // res.write(homePage);
+    res.write(html(homePage));
+    res.end();
+  } else if (url.pathname == "/about") {
+    // res.write(aboutPage);
+    res.write(html(aboutPage));
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.write(html(defaultPage));
+    res.end();
+  }
 });
 
 server.listen(3000);
+
+function html(body) {
+  return `<!DOCTYPE html>
+  <html lang="en">
+  <body>
+      ${body}
+  </body>
+  </html>`;
+}
