f2d3165dc432a14843c70cb6d319ecbba8dc5cae
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2022-10-23 21:35:57 +0100

    added addCat & addBreed pages

--- JS_BackEnd/01_Intro_NodeJS/01. Cat-Shelter-Workshop/resources/index.js
+++ JS_BackEnd/01_Intro_NodeJS/01. Cat-Shelter-Workshop/resources/index.js
@@ -8,6 +8,8 @@ const siteCss= require('./content/styles/site.js')
 const port = 3000;
 
 const homePage = require("./handlers/home.js");
+const addBreedPage = require('./handlers/addBreed.js')
+const addCatPage= require('./handlers/addCat.js')
 
 const server = http
   .createServer((req, res) => {
@@ -16,6 +18,17 @@ const server = http
         "content-type": "text/css",
       });
       res.write(siteCss)
+    }else if(req.url=='/cats/add-breed'){
+        res.writeHead(200,{
+            'content-type': 'text/html'
+        })
+        res.write(addBreedPage)
+    }else if(req.url=='/cats/add-cat'){
+        res.writeHead(200,{
+            'content-type': 'text/html'
+        })
+        res.write(addCatPage)   
+    
     } else {
       res.writeHead(200, {
         "Content-Type": "text/html",
