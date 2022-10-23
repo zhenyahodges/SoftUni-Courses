1fe3e65b42f7e5610d90261203205593400cc8b7
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2022-10-23 21:27:44 +0100

    added css

--- JS_BackEnd/01_Intro_NodeJS/01. Cat-Shelter-Workshop/resources/index.js
+++ JS_BackEnd/01_Intro_NodeJS/01. Cat-Shelter-Workshop/resources/index.js
@@ -1,27 +1,38 @@
-const http= require('http')
+const http = require("http");
 // const fs= require('fs')
 // const path= require('path')
 // const cats=require('./resources/data/cats.json')
 
-const port=3000
+const siteCss= require('./content/styles/site.js')
 
-const homePage= require('./handlers/home.js')
+const port = 3000;
 
-const server=http.createServer((req,res)=>{
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    })
+const homePage = require("./handlers/home.js");
 
-    res.write(homePage)
-    res.end()
-}).listen(port)
+const server = http
+  .createServer((req, res) => {
+    if (req.url == "/content/styles/site.css") {
+      res.writeHead(200, {
+        "content-type": "text/css",
+      });
+      res.write(siteCss)
+    } else {
+      res.writeHead(200, {
+        "Content-Type": "text/html",
+      });
+
+      res.write(homePage);
+    }
+    res.end();
+  })
+  .listen(port);
 
 // module.exports = (req, res) => {
 //     const pathname=url.parse(req.url).pathname
 //     const method=req.method
 
 //     if(pathname=='/' && method=='GET'){
-        
+
 //     }else{
 //         return true
 //     }
