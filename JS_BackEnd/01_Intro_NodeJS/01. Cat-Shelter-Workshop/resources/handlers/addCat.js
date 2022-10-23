44bdcf40a16c461f17186dcd89023ab2b9fc15f3
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2022-10-23 21:52:47 +0100

    update nav links

--- JS_BackEnd/01_Intro_NodeJS/01. Cat-Shelter-Workshop/resources/handlers/addCat.js
+++ JS_BackEnd/01_Intro_NodeJS/01. Cat-Shelter-Workshop/resources/handlers/addCat.js
@@ -14,9 +14,9 @@ module.exports=`<!DOCTYPE html>
     <header>
         <nav>
             <ul class="navigation">
-                <li><a href="">Home Page</a></li>
-                <li><a href="">Add Breed</a></li>
-                <li><a href="">Add Cat</a></li>
+            <li><a href="/">Home Page</a></li>
+            <li><a href="/cats/add-breed">Add Breed</a></li>
+            <li><a href="/cats/add-cat">Add Cat</a></li>
             </ul>
         </nav>
         <h1>Cat Shelter</h1>
