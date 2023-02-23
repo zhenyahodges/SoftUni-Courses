6b5be7910cf2378ae3d098da8ebd0a4cf9ca2b1b
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-02-23 19:13:15 +0000

    Update Counter.js

--- Front_End/REACT/REACT-PRACTICE/02-React-Components-Basic-Idea/components-basic-demo/src/components/Counter.js
+++ Front_End/REACT/REACT-PRACTICE/02-React-Components-Basic-Idea/components-basic-demo/src/components/Counter.js
@@ -3,14 +3,24 @@ import { useState } from "react";
 const Counter = (props) => {
   const [counter, setCounter] = useState(0);
 
-  const onButtonClick = (e) => {
+  const incremCounter = (e) => {
     setCounter((oldCOunter) => oldCOunter + 1);
+  };
+
+  const decremCounter = (e) => {
+    setCounter((oldCOunter) => oldCOunter - 1);
+  };
+
+  const clearCounter = (e) => {
+    setCounter(0);
   };
 
   return (
     <div>
       <h3>Counter: {counter}</h3>
-      <button onClick={onButtonClick}>+</button>
+      <button onClick={decremCounter}>-</button>
+      <button onClick={clearCounter}>Clear</button>
+      <button onClick={incremCounter}>+</button>
     </div>
   );
 };
