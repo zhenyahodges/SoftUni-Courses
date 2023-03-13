8dc38eac51c6f5375dbe9aa2688faef0e78b0170
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-03-13 10:40:24 +0000

    added change status, add new, loading

--- Front_End/REACT/REACT-PRACTICE/03-React-JS-Components-Deep-Dive/todo-list-resources/client/src/App.js
+++ Front_End/REACT/REACT-PRACTICE/03-React-JS-Components-Deep-Dive/todo-list-resources/client/src/App.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 
 import Footer from './components/Footer';
 import Header from './components/Header';
@@ -6,20 +6,41 @@ import Loading from './components/Loading';
 import TodoList from './components/TodoList';
 
 function App() {
+    const [todos, setTodos] = useState([]);
+    // prazen masiv st-st nachalna!!!!
+    const [isLoading,setIsLoading]=useState(true);
 
-const [todos,setTodos]=useState([]);
-// prazen masiv st-st nachalna!!!!
+    useEffect(() => {
+        fetch('http://localhost:3030/jsonstore/todos')
+            .then((res) => res.json())
+            .then((data) => {
+                const result = Object.keys(data).map((id) => ({
+                    id,
+                    ...data[id],
+                }));
+                setTodos(result);
+                setIsLoading(false);
+            });
+    }, []);
 
-  useEffect(()=>{
-    fetch('http://localhost:3030/jsonstore/todos')
-    .then(res=>res.json())
-    .then(data=>{
-      setTodos(Object.values(data));
-    });
-  },[]);
+    const onTodoAdd = () => {
+        const lastId = Number(todos[todos.length - 1].id);
+        const text = prompt('Task name:');
+        const newTask = { id: lastId + 1, text, isCompleted: false };
+        // setTodos(state => [...state, {id: lastId+1}]); na kraq
+        setTodos((state) => [newTask, ...state]);
+    };
+
+    const toggleTodoStatus = (id) => {
+        setTodos((state) =>
+            state.map((t) =>
+                t.id === id ? { ...t, isCompleted: !t.isCompleted } : t
+            )
+        );
+    };
 
     return (
-        <body>
+        <div>
             <Header />
             {/* <!-- Main content --> */}
             <main className='main'>
@@ -28,19 +49,28 @@ const [todos,setTodos]=useState([]);
                     <h1>Todo List</h1>
 
                     <div className='add-btn-container'>
-                        <button className='btn'>+ Add new Todo</button>
+                        <button className='btn' onClick={onTodoAdd}>
+                            + Add new Todo
+                        </button>
                     </div>
 
                     <div className='table-wrapper'>
-                        {/* <Loading></Loading> */}
+                     
+                        {isLoading
+                        ? <Loading/>
+                    :<TodoList
+                            todos={todos}
+                            toggleTodoStatus={toggleTodoStatus}
+                        />
+                     }
 
-                        <TodoList todos={todos}/>
+                        
                     </div>
                 </section>
             </main>
 
             <Footer />
-        </body>
+        </div>
     );
 }
 
--- Front_End/REACT/REACT-PRACTICE/03-React-JS-Components-Deep-Dive/todo-list-resources/client/src/components/TodoList.js
+++ Front_End/REACT/REACT-PRACTICE/03-React-JS-Components-Deep-Dive/todo-list-resources/client/src/components/TodoList.js
@@ -1,33 +1,27 @@
-export default function TodoList({
+import Todo from "./Todo";
+
+export default function TodoList({ 
   todos,
-}){
-    return(
-//    {/* <!-- Todo list table --> */}
-   <table className="table">
-   <thead>
-     <tr>
-       <th className="table-header-task">Task</th>
-       <th className="table-header-status">Status</th>
-       <th className="table-header-action">Action</th>
-     </tr>
-   </thead>
-   <tbody>
+  toggleTodoStatus,
+ }) {
+    return (
 
-    {todos.map(todo=>(
-        // {/* <!-- Todo item --> */}
-     <tr className="todo is-completed">
-     <td>{todo.text}</td>
-     <td>{todo.isCompleted ? 'Complete' : 'Not Complete'}</td>
-     <td className="todo-action">
-       <button className="btn todo-btn">Change status</button>
-     </td>
-   </tr>
-    ))
-    }
-
-   
-
-   </tbody>
- </table>
+        //    {/* <!-- Todo list table --> */}
+        <table className='table'>
+            <thead>
+                <tr>
+                    <th className='table-header-task'>Task</th>
+                    <th className='table-header-status'>Status</th>
+                    <th className='table-header-action'>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                {todos.map((todo) => 
+                <Todo key={todo.id} {...todo} toggleTodoStatus={toggleTodoStatus}/>
+                    // {/* <!-- Todo item --> */}
+                    
+                )}
+            </tbody>
+        </table>
     );
-};
+}
