f190a21c0adee4a40c67d767270d731c9c617806
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-03-17 14:17:46 +0000

    added nested routing

--- Front_End/REACT/REACT-PRACTICE/06-React-Router/react-router-demo/src/components/CharacterFilms.js
+++ Front_End/REACT/REACT-PRACTICE/06-React-Router/react-router-demo/src/components/CharacterFilms.js
@@ -7,6 +7,7 @@ export const CharacterFilms = () => {
     const { characterId } = useParams();
     const [films, setFilms] = useState([]);
 
+    // fetch like `people/${characterId}/films` from swapi,nut there is no such thing
     useEffect(() => {
         fetch(`${baseUrl}/films`)
             .then((res) => res.json())
@@ -18,10 +19,17 @@ export const CharacterFilms = () => {
     return (
         <>
             <h1>Character Films</h1>
-
-            {films.map((x) => (
-                <li key={x.url}>{x.title}</li>
-            ))}
+            {films.map((x) => {
+                const id = x.url
+                    .split('/')
+                    .filter((x) => x)
+                    .pop();
+                return (
+                    <li key={id}>
+                        <Link to={`/films/${id}`}>{x.title}</Link>
+                    </li>
+                );
+            })}           
         </>
     );
 };
