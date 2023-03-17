452d81ec96eb8ed448fde37a4f0f80179fac5f0c
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-03-17 17:20:01 +0000

    started the architecture

---
+++ Front_End/REACT/REACT-PRACTICE/07-ReactJS-Routing-Workshop/games-play-resources/games-play-demo/src/App.js
@@ -0,0 +1,13 @@
+import { Footer } from './components/Footer/Footer';
+import { Header } from './components/Header/Header';
+
+function App() {
+    return (
+        <div id='box'>
+            <Header />
+            <Footer/>
+        </div>
+    );
+}
+
+export default App;
---
+++ Front_End/REACT/REACT-PRACTICE/07-ReactJS-Routing-Workshop/games-play-resources/games-play-demo/src/components/Footer/Footer.js
@@ -0,0 +1,5 @@
+export const Footer=()=>{
+return(
+    <p>All rights reserved 2023</p>
+);
+};
