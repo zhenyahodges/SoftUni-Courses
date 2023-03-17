3672b456adec9a2d025296300a2a1620c31727f4
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-03-17 17:44:27 +0000

    finished architec for now, start on router

--- Front_End/REACT/REACT-PRACTICE/07-ReactJS-Routing-Workshop/games-play-resources/games-play-demo/src/App.js
+++ Front_End/REACT/REACT-PRACTICE/07-ReactJS-Routing-Workshop/games-play-resources/games-play-demo/src/App.js
@@ -1,11 +1,25 @@
 import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
+import { Home } from './components/Home/Home';
+import { Login } from './components/Login/Login';
+import { Register } from './components/Register/Register';
+import { CreateGame } from './components/CreateGame/CreateGame';
+import { Catalog } from './components/Catalog/Catalog';
 
 function App() {
     return (
         <div id='box'>
             <Header />
-            <Footer/>
+            {/* <!-- Main Content --> */}
+            <main id='main-content'>
+              {/* <Home></Home> */}
+              {/* <Login/> */}
+              {/* <Register/> */}
+              {/* <CreateGame/> */}
+              {/* <Catalog/> */}
+            </main>
+      
+            <Footer />
         </div>
     );
 }
