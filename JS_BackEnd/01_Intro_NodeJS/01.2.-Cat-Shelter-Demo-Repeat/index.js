a14bac4872f2ec2278d8528af8256e227c6af86d
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-06-10 15:39:23 +0100

    data

---
+++ JS_BackEnd/01_Intro_NodeJS/01.2.-Cat-Shelter-Demo-Repeat/index.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const homePage = require('./views/home');
+const siteCss = require('./styles/site');
+const addCatPage = require('./views/addCat');
+const cats = require('./cats.json');
+
+const catTemplate = (cat) => `
+<li>
+<img src="${cat.imageUrl}">
+<h3>${cat.name}</h3>
+<p><span>Breed: </span>${cat.breed}t</p>
+<p><span>Description: </span>${cat.description}</p>
+<ul class="buttons">
+    <li class="btn edit"><a href="">Change Info</a></li>
+    <li class="btn delete"><a href="">New Home</a></li>
+</ul>
+</li>`;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/html',
+    });
+
+    if (req.url == '/styles/site.css') {
+        console.log('sty;les');
+        res.writeHead(200, {
+            'Content-Type': 'text/css',
+        });
+        res.write(siteCss);
+    } else if (req.url == '/cats/add-cat') {
+        res.write(addCatPage);
+    } else {
+        const homePageResult = homePage.replace(
+            '{{cats}}',
+            cats.map((x) => catTemplate(x)).join('')
+            // cats.map(catTemplate)
+        );
+        res.write(homePageResult);
+    }
+
+    res.end();
+});
+
+server.listen(5000, () => console.log('Server is listening on port 5000...'));
