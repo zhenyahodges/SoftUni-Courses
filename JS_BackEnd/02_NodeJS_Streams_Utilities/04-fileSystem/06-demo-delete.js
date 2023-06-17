293961d9f2a7a79776f0cbe2486886e636ee8bbf
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-06-17 20:39:46 +0100

    updates

--- JS_BackEnd/02_NodeJS_Streams_Utilities/04-fileSystem/06-demo-delete.js
+++ JS_BackEnd/02_NodeJS_Streams_Utilities/04-fileSystem/06-demo-delete.js
@@ -1,6 +1,8 @@
 const fs = require('fs/promises');
 
 
-fs.unlink('./04-fileSystem/text2.txt').then(() => {
+fs.unlink('./04-fileSystem/text.txt').then(() => {
     console.log('finished deleting');
 });
+
+
