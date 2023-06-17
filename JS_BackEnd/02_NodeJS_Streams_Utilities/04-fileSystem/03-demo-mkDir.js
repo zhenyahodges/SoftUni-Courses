bd56fbd085fd34c3345eed2d5c06b5c5e06df636
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-06-17 20:26:04 +0100

    updates

--- JS_BackEnd/02_NodeJS_Streams_Utilities/04-fileSystem/03-demo-mkDir.js
+++ JS_BackEnd/02_NodeJS_Streams_Utilities/04-fileSystem/03-demo-mkDir.js
@@ -1,5 +1,16 @@
 const fs = require('fs/promises');
 
+// create directory
+
 fs.mkdir('./test').then(() => {
+    console.log('finished');
+});
+
+fs.mkdir('./test2',err=>{
+    if(err){
+        console.log(err);
+        return;
+    }
+}).then(() => {
     console.log('finished');
 });
