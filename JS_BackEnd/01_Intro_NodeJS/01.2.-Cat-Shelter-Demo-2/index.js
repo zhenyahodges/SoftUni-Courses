86dbf654a2c09e1355c6e05532afef265d8d2df1
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-06-17 17:37:39 +0100

    added fs

--- JS_BackEnd/01_Intro_NodeJS/01.2.-Cat-Shelter-Demo-2/index.js
+++ JS_BackEnd/01_Intro_NodeJS/01.2.-Cat-Shelter-Demo-2/index.js
@@ -1,8 +1,10 @@
 const http = require('http');
+const fs = require('fs/promises');
+
 const homePage = require('./views/home');
 const siteCss = require('./styles/site');
 const addCatPage = require('./views/addCat');
-const cats = require('./cats.json');
+// const cats = require('./cats.json');
 
 const catTemplate = (cat) => `
 <li>
@@ -23,26 +25,28 @@ const server = http.createServer((req, res) => {
         'Content-Type': 'text/html',
     });
 
-    // if (req.url == '/styles/site.css') {
     if (url.pathname == '/styles/site.css') {
-        // console.log('sty;les');
         res.writeHead(200, {
             'Content-Type': 'text/css',
         });
         res.write(siteCss);
-        // } else if (req.url == '/cats/add-cat') {
+        res.end();
+
     } else if (url.pathname == '/cats/add-cat') {
         res.write(addCatPage);
+        res.end();
     } else {
-        const homePageResult = homePage.replace(
-            '{{cats}}',
-            cats.map((x) => catTemplate(x)).join('')
-            // cats.map(catTemplate)
-        );
-        res.write(homePageResult);
+        fs.readFile('./cats.json').then((text) => {
+            let cats = JSON.parse(text);
+            const homePageResult = homePage.replace(
+                '{{cats}}',
+                cats.map((x) => catTemplate(x)).join('')
+                // cats.map(catTemplate)
+            );
+            res.write(homePageResult);
+            res.end();
+        });
     }
-
-    res.end();
 });
 
 server.listen(5000, () => console.log('Server is listening on port 5000...'));
