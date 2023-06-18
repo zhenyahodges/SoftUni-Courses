b39c6d84bc13cbbf1654a313e2775bad71d60072
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-06-18 22:16:43 +0100

    hbs and json

--- JS_BackEnd/03-1-ExpressJS-Templating-Workshop/Demo-Workshop-Cubicle-part1/src/index.js
+++ JS_BackEnd/03-1-ExpressJS-Templating-Workshop/Demo-Workshop-Cubicle-part1/src/index.js
@@ -1,11 +1,23 @@
 const express=require('express');
+const handlebars= require('express-handlebars');
 
 const app= express();
-
+// 1-hbs
 app.use('/static', express.static('public'));
+// 2-hbs
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+// 3-hbs
+app.set('view engine', 'hbs');
+
+// 4-views change
+// app.set('views', './src/views');
+// 4+views=>layouts folder=> main.hbs file
+// 2nd option=> json change
 
 app.get('/',(req, res)=>{
-    res.send('Hello, world')
+    res.render('index')
 });
 
 app.listen(5000, ()=>console.log('Server listening on port 5000...'));
