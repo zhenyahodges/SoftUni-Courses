0569c157878c401fe563dc1fd3081587078f2630
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-06-18 11:26:40 +0100

    router responses, json debug setup nodemon

--- JS_BackEnd/03-ExpressJS-Templating-2/index.js
+++ JS_BackEnd/03-ExpressJS-Templating-2/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
 const cats = [];
-
 
 // action
 
@@ -20,32 +20,67 @@ app.get('/cats', (req, res) => {
 });
 
 // not used often regexp validation for params
-app.get('/cats/:catId(\\d+)',(req,res) => {
-    let catId= Number(req.params.catId);
+app.get('/cats/:catId(\\d+)', (req, res) => {
+    let catId = Number(req.params.catId);
     res.send(cats[catId]);
-})
+});
 
 // app.get('/cats/:catName',(req,res) => {
 // //    TODO:
 // })
 
-app.post('/cats/:catName',(req,res)=>{
+app.post('/cats/:catName', (req, res) => {
     // TODO: implem posting cats
-    const cat=req.params.catName;
+    const cat = req.params.catName;
 
     cats.push(cat);
     // console.log(cats);
-    res.status(201)
+    res.status(201);
     res.send(`Added ${cat} to the collection`);
 });
 
-app.put('/cats',(req,res)=>{
+app.put('/cats', (req, res) => {
     // TODO: implement
     res.send('Modify collection');
 });
 
+// default way(no express)
+app.get('/download', (req, res) => {
+    res.writeHead(200, {
+        // 'content-disposition': 'attachment; fileName="sample.pdf"'
+        'content-type': 'application/pdf',
+        'content-disposition': 'inline',
+        // to open within the page
+    });
+
+    const readStream = fs.createReadStream('sample.pdf');
+    // readStream.on('data', (data) => {
+    //     res.write(data);
+    // });
+    // readStream.on('end', () => {
+    //     res.end();
+    // });
+    readStream.pipe(res);
+
+    // res.download('');
+});
+
+app.get('/expdownload', (req, res) => {
+    res.download('sample.pdf');
+    // also
+    // res.attachments('sample.pdf');
+    // doesn't add end,so can send add things
+
+    // res.sendFile('sample.pdf');
+    // s callback added
+});
+
+app.get('/[0-9]+/', (req, res) => {
+    res.send('Only number route');
+});
+
 // app.get('*',(req,res)=>{
-app.all('*',(req,res)=>{
+app.all('*', (req, res) => {
     res.status(404);
     res.send('Ahhhhhh! Something went wrong');
 });
