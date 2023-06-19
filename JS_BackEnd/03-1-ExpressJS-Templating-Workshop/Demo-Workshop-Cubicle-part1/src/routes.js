9ecdd3ddc7f9826ba7dadf87ca6df7d6b713ca7c
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-06-19 10:17:27 +0100

    Update routes.js

--- JS_BackEnd/03-1-ExpressJS-Templating-Workshop/Demo-Workshop-Cubicle-part1/src/routes.js
+++ JS_BackEnd/03-1-ExpressJS-Templating-Workshop/Demo-Workshop-Cubicle-part1/src/routes.js
@@ -12,6 +12,7 @@ const router=express.Router();
 
 router.get('/', homeController.index);
 router.get('/about', homeController.about);
+// or
 router.use('/cube',cubeController);
 
 module.exports = router;
