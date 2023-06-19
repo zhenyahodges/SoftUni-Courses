2be34c4a8b9dbba718162a3d0f94643c29bcdb39
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-06-19 13:25:44 +0100

    search

--- JS_BackEnd/03-1-ExpressJS-Templating-Workshop/Demo-Workshop-Cubicle-part1/src/services/cubeService.js
+++ JS_BackEnd/03-1-ExpressJS-Templating-Workshop/Demo-Workshop-Cubicle-part1/src/services/cubeService.js
@@ -15,7 +15,9 @@ exports.save = (cube) => {
 };
 
 exports.getOne = (cubeId) => cubes[cubeId];
-exports.getAll = (search,from,to) =>{
-    const result=cubes.filter(x=>x.name.toLowerCase().includes(search?.toLowerCase() || ''));
+exports.getAll = (search='',from=0,to=6) =>{
+    const result=cubes
+    .filter(x=>x.name.toLowerCase().includes(search?.toLowerCase() || ''))
+    .filter(x=>x.difficultyLevel>=from && x.difficultyLevel<=to);
     return result;
 }
