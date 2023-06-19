0e08bc6c4d6e82765ecb546905b7a55979213290
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-06-19 10:02:18 +0100

    render cubes on home page

--- JS_BackEnd/03-1-ExpressJS-Templating-Workshop/Demo-Workshop-Cubicle-part1/src/controllers/homeController.js
+++ JS_BackEnd/03-1-ExpressJS-Templating-Workshop/Demo-Workshop-Cubicle-part1/src/controllers/homeController.js
@@ -1,5 +1,8 @@
+const cubes = require('../db.json');
+
 exports.index = (req, res) => {
-    res.render('index');
+    res.render('index', { cubes });
+    // cubes:cubes shorthand=> cubes
 };
 
 exports.about = (req, res) => {
