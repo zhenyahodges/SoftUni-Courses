f2fe9c676a391a852d915a7b2154399a26eacfe5
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-06-20 09:18:28 +0100

    mongoose models, virtual props

--- JS_BackEnd/04_NoSQL_MongoDB/02-demo-mongoose/controllers/userController.js
+++ JS_BackEnd/04_NoSQL_MongoDB/02-demo-mongoose/controllers/userController.js
@@ -1,17 +1,27 @@
-const router=require('express').Router();
-const {User}=require('../models/User');
+const router = require('express').Router();
+const { User } = require('../models/User');
 
-router.get('/', async (req,res)=>{
-    const users=await User.find().lean();
+router.get('/', async (req, res) => {
+    // const users=await User.find().lean();
+    // lean cant use with methods,so:
+    const users= await User.find();
+
+    users.forEach((user) => {
+        console.log(user.getInfo());
+        console.log(user.isNew);
+    });
     // console.log(users);
-    res.render('users', {users});
+
+    // res.render('users', { users });
+
+    res.render('users', { users: []});
 });
 
-router.get('/create',  (req,res)=>{   
+router.get('/create', (req, res) => {
     res.render('createUser');
-})
+});
 
-router.post('/create',async (req,res)=>{
+router.post('/create', async (req, res) => {
     // console.log(req.body);
 
     // ==> option 1 -create db doc
@@ -20,9 +30,9 @@ router.post('/create',async (req,res)=>{
     // // console.log(savedUser);
 
     // ==> option 2 -create db doc
-    let savedUser=await User.create(req.body);
+    let savedUser = await User.create(req.body);
 
     res.redirect('/users');
-})
+});
 
 module.exports = router;
--- JS_BackEnd/04_NoSQL_MongoDB/02-demo-mongoose/models/User.js
+++ JS_BackEnd/04_NoSQL_MongoDB/02-demo-mongoose/models/User.js
@@ -1,18 +1,51 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+    // not compulsory
     firstName: String,
-   lastName: String,
+
+    // compulsory
+    // firstname: {
+    //     type: String,
+    //     required: true,
+    // },
+    lastName: String,
     email: String,
     imageUrl: String,
-    phoneNUmber: String,
+    phoneNumber: String,
     // address: {
     //     streetNumber: Number,
     //     street: String,
     //     city: String,
     //     country: String
     // }
+
+    // compulsory
+    // firstname:{
+    //     type: String,
+    //     required: true,
+    // }
 });
+
+// METHODS
+// NB! FUNCTION NOT arrow b/c of this
+
+// opt1
+// userSchema.method('getinfo', function(){
+//     return `${this.firstName}-${this.lastName || 'n/a'}`;
+
+// })
+
+// opt2
+userSchema.methods.getInfo = function () {
+    return `${this.firstName}-${this.lastName || 'n/a'}`;
+};
+
+userSchema.virtual('isNew')
+.get(function (){
+    return this.phoneNumber>3
+});
+// .set...
 
 const User = mongoose.model('User', userSchema);
 
