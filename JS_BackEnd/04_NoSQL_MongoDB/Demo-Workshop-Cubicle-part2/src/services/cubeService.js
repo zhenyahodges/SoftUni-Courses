a4494916984a356b9d4896b65e893866f381c999
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-06-21 15:35:01 +0100

    many ot many db connect save

--- JS_BackEnd/04_NoSQL_MongoDB/Demo-Workshop-Cubicle-part2/src/services/cubeService.js
+++ JS_BackEnd/04_NoSQL_MongoDB/Demo-Workshop-Cubicle-part2/src/services/cubeService.js
@@ -1,14 +1,15 @@
 const fs = require('fs/promises');
 const path = require('path');
-const Cube=require('../models/Cube')
+const Cube = require('../models/Cube');
+const Accessory = require('../models/Accessory');
 
 exports.getOne = (cubeId) => Cube.findById(cubeId);
 
 exports.getAll = async (search = '', fromInput, toInput) => {
-    let cubes=await Cube.find().lean();
+    let cubes = await Cube.find().lean();
     // const from = Number(fromInput) || 0;
     // const to = Number(toInput) || 6;
-    
+
     // const result = cubes
     //     .filter((x) =>
     //         x.name.toLowerCase().includes(search?.toLowerCase() || '')
@@ -20,7 +21,14 @@ exports.getAll = async (search = '', fromInput, toInput) => {
 
 exports.create = (cube) => Cube.create(cube);
 
-exports.attachAccessory=(cubeId, accessoryId)=>{
-    console.log(cubeId);
-    console.log(accessoryId);
-}
+exports.attachAccessory = async (cubeId, accessoryId) => {
+    const cube = await Cube.findById(cubeId);
+    const accessory = await Accessory.findById(accessoryId);
+
+    cube.accessories.push(accessory);
+    accessory.cubes.push(cube);
+
+    await cube.save();
+    await accessory.save();
+    return cube;
+};
