46a30bd02bd973e456cb7998ae5ee23244a276b7
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-06-21 17:09:32 +0100

    Update cubeService.js

--- JS_BackEnd/04_NoSQL_MongoDB/Demo-Workshop-Cubicle-part2/src/services/cubeService.js
+++ JS_BackEnd/04_NoSQL_MongoDB/Demo-Workshop-Cubicle-part2/src/services/cubeService.js
@@ -7,15 +7,6 @@ exports.getOne = (cubeId) => Cube.findById(cubeId).populate('accessories');
 
 exports.getAll = async (search = '', fromInput, toInput) => {
     let cubes = await Cube.find().lean();
-    // const from = Number(fromInput) || 0;
-    // const to = Number(toInput) || 6;
-
-    // const result = cubes
-    //     .filter((x) =>
-    //         x.name.toLowerCase().includes(search?.toLowerCase() || '')
-    //     )
-    //     .filter((x) => x.difficultyLevel >= from && x.difficultyLevel <= to);
-    // return result;
     return cubes;
 };
 
