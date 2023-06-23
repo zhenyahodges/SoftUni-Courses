ba9429834b0d63d57f4785142edbcf21d45cda48
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-06-23 12:12:00 +0100

    Update index.js

--- JS_BackEnd/05-Sessions-and-Authentication/demo-sessions-auth/index.js
+++ JS_BackEnd/05-Sessions-and-Authentication/demo-sessions-auth/index.js
@@ -1,18 +1,30 @@
 const express = require('express');
-const cookieParser=require('cookie-parser');
-const bcrypt=require('bcrypt');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
 
 const app = express();
+const password = 'mysecretpassword';
+// NB!
+const saltRounds = 10;
+const hashedPassword ='$2b$10$tQW2Nrx1Gg8NSJO1qvf9C.P8RLKrTwJ3ejNHIOYkBOskDh133B03u'
 
 app.use(cookieParser());
 
-app.get('/login/:password', (req, res) => {   
- 
+app.get('/hash/:password?', async (req, res) => {
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashs = await bcrypt.hash(req.params.password, salt);
+
+    console.log(salt);
+    console.log(hashs);
     res.send('Hello, world!');
 });
 
+app.get('/login/:password?', async (req, res) => {
+    const isValidPass = await bcrypt.compare(req.params.password, hashedPassword);
+    isValidPass? res.send('Successful login') : res.send('Invalid pass');
+});
+
 app.get('/cats', (req, res) => {
-  
     res.send('Cats World');
 });
 
