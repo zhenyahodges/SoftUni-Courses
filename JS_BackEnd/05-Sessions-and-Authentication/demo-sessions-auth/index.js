81890afad5a67a29155d1652137a0bc1129fe437
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-06-24 09:12:11 +0100

    register

--- JS_BackEnd/05-Sessions-and-Authentication/demo-sessions-auth/index.js
+++ JS_BackEnd/05-Sessions-and-Authentication/demo-sessions-auth/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const hbs = require('express-handlebars');
+const bcrypt = require('bcrypt');
+const userSessions={
 
+}
 const app = express();
+const saltRounds=10;
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/static', express.static('public'));
@@ -23,13 +27,35 @@ app.get('/register', (req, res) => {
     res.render('register');
 });
 
-app.post('/register', (req, res) => {
+app.post('/register',async (req, res) => {
+    const {email,password}=req.body
+    if(userSessions[email]){
+        res.status(400).send('User allready exists')
+    }
+
+    const hash= await bcrypt.hash(password, saltRounds)
+    userSessions[email]={
+        email,
+        password: hash,
+    }
     console.log(req.body);
+    res.redirect('/login');
 });
 
 app.get('/login', (req, res) => {
     res.render('login');
 });
+
+app.post('/login',async (req,res)=>{
+    const {email,password}=req.body;
+   const isAuth= await bcrypt.compare(password, userSessions[email].password);
+    // todo validations
+    if(isAuth){
+        res.redirect('/');
+    }else{
+        res.status(401).send('Wron username or password');
+    }
+})
 
 app.listen(5000, () => {
     console.log('Server listening on port 5000...');
