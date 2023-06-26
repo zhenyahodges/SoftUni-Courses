ee7b4c6def788ff4986f6a101c719bd37900be4a
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-06-27 00:02:03 +0100

    Update User.js

--- JS_BackEnd/06-Validation-and-Error-Handling/Demo-Workshop-Cubicle-part4/src/models/User.js
+++ JS_BackEnd/06-Validation-and-Error-Handling/Demo-Workshop-Cubicle-part4/src/models/User.js
@@ -8,28 +8,31 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: [true, 'Username is required'],
+        unique: [true, 'email should be uqnique'],
+        validate: /[a-zA-Z0-9]/,
+        minLength: 5,
     },
     password: {
         type: String,
-        required: true,
+        required: [true, 'Password is required'],
+        minLength: 8,
+        validate: /[a-zA-Z0-9]/,
     },
 });
 
-userSchema.virtual('repeatPassword').set(function(value){
-    if(this.password!==value){
-        throw new Error('Passwords do not match')
+userSchema.virtual('repeatPassword').set(function (value) {
+    if (this.password !== value) {
+        throw new Error('Passwords do not match');
     }
 });
 
 // hashed pass in model
-userSchema.pre('save', function(next){
-  bcrypt.hash(this.password, saltRounds)
-  .then(hashedPassword => {
-    this.password=hashedPassword;
-    next();
-  })
-
-})
+userSchema.pre('save', function (next) {
+    bcrypt.hash(this.password, saltRounds).then((hashedPassword) => {
+        this.password = hashedPassword;
+        next();
+    });
+});
 
 const User = mongoose.model('User', userSchema);
 
