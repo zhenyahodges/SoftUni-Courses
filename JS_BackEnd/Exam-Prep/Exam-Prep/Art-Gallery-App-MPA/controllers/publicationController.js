886ac2b1a23e12b8001716be34ffc2fa51d5ff31
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-07-01 11:24:39 +0100

    create publicataions and gallery view

--- JS_BackEnd/Exam-Prep/Exam-Prep/Art-Gallery-App-MPA/controllers/publicationController.js
+++ JS_BackEnd/Exam-Prep/Exam-Prep/Art-Gallery-App-MPA/controllers/publicationController.js
@@ -2,7 +2,11 @@ const router = require('express').Router();
 const { isAuth } = require('../middlewares/authMiddleware');
 const publicationService = require('../services/publicationService');
 
-router.use(isAuth);
+router.get('/', async(req, res)=>{
+    const publications= await publicationService.getAll().lean();
+
+    res.render('publication', {publications})
+})
 
 router.get('/create', isAuth, (req, res) => {
     res.render('publication/create');
@@ -12,7 +16,7 @@ router.post('/create', isAuth, async (req, res) => {
     const publicationData = { ...req.body, author: req.user._id };
     await publicationService.create(publicationData);
 
-    res.redirect('/');
+    res.redirect('/publications');
 });
 
 module.exports = router;
--- JS_BackEnd/Exam-Prep/Exam-Prep/Art-Gallery-App-MPA/services/publicationService.js
+++ JS_BackEnd/Exam-Prep/Exam-Prep/Art-Gallery-App-MPA/services/publicationService.js
@@ -1,3 +1,4 @@
 const Publication = require('../models/Publication');
 
+exports.getAll =()=> Publication.find();
 exports.create = (publicationData) => Publication.create(publicationData);
