257490b33b63a02ac2b2bba0a53535e2f05afb7e
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-07-02 12:12:40 +0100

    Update homeController.js

--- JS_BackEnd/Exam-Prep/Exam-Prep/Art-Gallery-App-MPA/controllers/homeController.js
+++ JS_BackEnd/Exam-Prep/Exam-Prep/Art-Gallery-App-MPA/controllers/homeController.js
@@ -2,13 +2,11 @@ const router = require('express').Router();
 const publicationService = require('../services/publicationService');
 
 router.get('/', async (req, res) => {
-    const publicationResult = await publicationService
-        .getAll()
-        .lean()
-        .map((x) => ({
-            ...x,
-            shareCount: x.usersShared.length,
-        }));
+    const publicationResult = await publicationService.getAll().lean();
+    const publications = publicationResult.map((x) => ({
+        ...x,
+        shareCount: x.usersShared.length,
+    }));
     res.render('home', { publications });
 });
 
