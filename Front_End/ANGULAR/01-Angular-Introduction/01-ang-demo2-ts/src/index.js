dc61e1cabcdc31355fbf5fb0c137771795540269
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-07-09 14:28:10 +0100

    typesript demos

--- Front_End/ANGULAR/01-Angular-Introduction/01-ang-demo2-ts/src/index.js
+++ Front_End/ANGULAR/01-Angular-Introduction/01-ang-demo2-ts/src/index.js
@@ -1,12 +1,8 @@
 // let username='Ivan';
-
 // console.log(username);
-
 // d2.
-
 // let isOpen: boolean=false;
 // let isOpenArray: boolean[]=[false];
-
 // function createUser(username: string, age?:number) {
 //     // age:number=0
 //     return {
@@ -14,57 +10,40 @@
 //         age
 //     };
 // }
-
 // const ivan = createUser('Ivan',100);
-
 // d3.
-function id<T>(item: T): T {
+function id(item) {
     return item;
 }
-
-interface IMyDto {
-    prop: string;
-    prop1: number;
-}
-
-// types
-type MyDtoOrNumber = IMyDto | number;
-// type BooleanArray=boolean[];
-type BooleanArray = Array<boolean>;
-
-let num = id<MyDtoOrNumber>({ prop: '1', prop1: 1 });
+var num = id({ prop: '1', prop1: 1 });
 // let num = id<IMyDto| number>({ prop: '1', prop1: 1 });
 // let num = id<IMyDto>({ prop: '1', prop1: 1 });
-
 // typisation?!
 // let num=id<{prop: string; prop1: number}>({prop: '1', prop1: 1});
-
 // let num=id('1');
 // const num=id('1');
-
 // let num=id(1);
 // const num=id(1);
 // const num=id<number>(1);
-
 // ========CLASSES=============
 // class MyClass implements IMyDto{
 //     prop: string;
 //     prop1: number;
 // }
 // =======================
-class MyClass {
-    constructor(public name: string, private age: number) {}
-}
-
+var MyClass = /** @class */ (function () {
+    function MyClass(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    return MyClass;
+}());
 // class MyClassJS {
 //     constructor(name, age) {
 //         this.name = name;
 //         this.age = age;
 //     }
 // }
-
-const ivan = new MyClass('Ivan', 20);
-
+var ivan = new MyClass('Ivan', 20);
 // NB! can still use private as follows
 // (ivan as any).age=1000;
-
