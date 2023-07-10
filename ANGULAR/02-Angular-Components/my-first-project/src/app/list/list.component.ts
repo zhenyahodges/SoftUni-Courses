ace5a940fe962b9ec31571ef42036dabb1963587
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-07-10 11:33:44 +0100

    template

--- ANGULAR/02-Angular-Components/my-first-project/src/app/list/list.component.ts
+++ ANGULAR/02-Angular-Components/my-first-project/src/app/list/list.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent {
+  
+  users=[
+    {
+      firstName: 'Ivan',
+      lastName: 'Ivanov'
+    },
+    {
+      firstName: 'Petar',
+      lastName: 'Petrov'
+    }
+  ]
 
+  constructor() {
+    
+  }
 }
