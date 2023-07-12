2c96d1d40f573376d03fbf87e057d07053866389
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-07-12 18:16:08 +0100

    static string and number

--- ANGULAR/02-Angular-Components/my-first-project/src/app/list-item/list-item.component.ts
+++ ANGULAR/02-Angular-Components/my-first-project/src/app/list-item/list-item.component.ts
@@ -11,5 +11,6 @@ export class ListItemComponent {
   @Input() showLastName!: boolean;
 
   // @Input() staticString!: string;
-  @Input() staticString!: number;
+  @Input() staticString!: string;
+  @Input() staticNumber!: number;
 }
