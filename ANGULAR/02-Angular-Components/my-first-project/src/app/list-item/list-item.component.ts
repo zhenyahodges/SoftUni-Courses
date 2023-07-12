603fda26fdf889cd62679f6ec36ec29448812395
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-07-12 21:20:21 +0100

    lifesstyle hooks

--- ANGULAR/02-Angular-Components/my-first-project/src/app/list-item/list-item.component.ts
+++ ANGULAR/02-Angular-Components/my-first-project/src/app/list-item/list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output,OnDestroy } from '@angular/core';
 // dekorirame s operator input za da vzemem data za users
 
 export interface ICustomEvent {
@@ -10,7 +10,8 @@ export interface ICustomEvent {
   templateUrl: './list-item.component.html',
   styleUrls: ['./list-item.component.scss'],
 })
-export class ListItemComponent {
+
+export class ListItemComponent implements OnInit, OnDestroy{
   @Input() user!: { firstName: string; lastName: string };
   @Input() showLastName!: boolean;
 
@@ -20,10 +21,34 @@ export class ListItemComponent {
 
   @Output('myCustomEvent') customEvent = new EventEmitter<ICustomEvent>();
 
-  constructor() {}
+  intervalId: number | undefined;
+
+  constructor() {
+    console.log(this.user);
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId);
+  }
+
+  ngOnInit() {    
+      this.intervalId = setInterval(() => {
+    },5000) as unknown as number;
+  }
 
   selectClickHandler($event: MouseEvent) {
     $event.stopPropagation();
     this.customEvent.emit({ test: 123 });
   }
 }
+
+// class Person{
+//   name!: string;
+
+//   ngOnInit(){
+//     console.log(this.name);
+//   }
+// }
+// const ivan=new Person()
+// ivan.name='Ivan'
+// ivan.ngOnInit()
