5e6852d30b30573e59032ef75a925071b9162d70
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-07-18 10:48:14 +0100

    Update index.js

--- JS_BackEnd/01_Intro_NodeJS/demo3-v/index.js
+++ JS_BackEnd/01_Intro_NodeJS/demo3-v/index.js
@@ -1,38 +1,67 @@
 const http = require('http');
 
-const homePage = `<!DOCTYPE html>
-<html lang="en">
-<body>
-<a href="/about">About</a>
-<h1>Home</h1>  
-</body>
-</html>`;
+const homePage = `
+<h1>Home</h1>`;
 
-const aboutPage = `<!DOCTYPE html>
-<html lang="en">
-<body>  
-<a href="/">Home</a>  
-<h1>About</h1>
-</body>
-</html>`;
+const aboutPage = `
+<h1>About</h1>`;
+
+const catalogPage = `
+<h1>Catalog</h1>`;
+
+const defaultPage = `
+<h1>404 Not Found</h1>`;
+
+const routes = {
+    '/': homePage,
+    '/about': aboutPage,
+    '/catalog': catalogPage,
+};
 
 const server = http.createServer((req, res) => {
-    console.log('Req received');
+    console.log('Req received', req.method, req.url);
 
     const url = new URL(req.url, `http://${req.headers.host}`);
     console.log(url);
 
-    if (req.url == '/') {
-        // res.writeHead(200, ['Content-Type', 'text-html']);
-        res.write(homePage);
-        res.end();
-    } else if (req.url == '/about') {
-        res.write(aboutPage);
+    const page = routes[url.pathname];
+
+    if (page != undefined) {
+        res.write(html(page));
         res.end();
     } else {
         res.statusCode = 404;
+        res.write(html(defaultPage));
         res.end();
     }
+    // if (req.url == '/') {
+    // if (url.pathname == '/') {
+    //     // res.writeHead(200, ['Content-Type', 'text-html']);
+    //     res.write(html(homePage));
+    //     res.end();
+    // } else if (url.pathname == '/about') {
+    //     res.write(html(aboutPage));
+    //     res.end();
+    // } else {
+    //     res.statusCode = 404;
+    //     res.write(html(defaultPage));
+    //     res.end();
+    // }
 });
 
 server.listen(3000);
+
+function html(body) {
+    return `<!DOCTYPE html>
+<html lang="en">
+<body>  
+    <nav>
+        <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        </ul>
+    </nav>
+${body}
+</body>
+</html>`;
+}
