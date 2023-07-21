12442f5118223de14e2b431475d73c411f6bddc9
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-07-21 11:25:13 +0100

    Update fs-demo.js

--- JS_BackEnd/02_NodeJS_Streams_Utilities/demo-v/fs-demo.js
+++ JS_BackEnd/02_NodeJS_Streams_Utilities/demo-v/fs-demo.js
@@ -1,17 +1,34 @@
 // const fs = require('fs')
 
-// fs.readFile('./demo.txt', (err,data)=>{
+// fs.readFileSync('./demo.txt', (err,data)=>{
 //     if(err!=null){
 //         return console.log(err.message);
 //     }
 //     console.log(data.toString());
 // });
 
-const fs = require('fs').promises;
+// promises
 
-start();
+// const fs = require('fs').promises;
 
-async function start() {
-    const data = await fs.readFile('./demo.txt');
-    console.log(data.toString());
+// start();
+
+// async function start() {
+//     const data = await fs.readFile('./demo.txt');
+//     console.log(data.toString());
+// }
+
+//
+
+const fs = require('fs');
+
+const result = fs.readdirSync('.');
+
+for(let item of result) {
+    if(fs.statSync(`./${item}`).isDirectory()){
+        console.log(item, 'is a directory');
+    }else{
+        console.log(item,'is a file');
+    }
 }
+
