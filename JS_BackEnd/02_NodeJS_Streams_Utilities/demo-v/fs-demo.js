953ac79ecb14a51daeb37aa70b52b7138753454b
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-07-21 11:08:31 +0100

    fs promises

--- JS_BackEnd/02_NodeJS_Streams_Utilities/demo-v/fs-demo.js
+++ JS_BackEnd/02_NodeJS_Streams_Utilities/demo-v/fs-demo.js
@@ -1,9 +1,17 @@
-const fs = require('fs');
+// const fs = require('fs')
 
-fs.readFile('./demo.txt', (err,data)=>{
-    if(err!=null){
-        return console.log(err.message);
-    }
+// fs.readFile('./demo.txt', (err,data)=>{
+//     if(err!=null){
+//         return console.log(err.message);
+//     }
+//     console.log(data.toString());
+// });
+
+const fs = require('fs').promises;
+
+start();
+
+async function start() {
+    const data = await fs.readFile('./demo.txt');
     console.log(data.toString());
-});
-
+}
