dfbd0e30bd0023dd4813c16bc05cb1ee91ea9629
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-07-22 21:23:18 +0100

    init setup

---
+++ JS_BackEnd/04_NoSQL_MongoDB/04-demo-v/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const hbs = require('express-handlebars').create({
+    extname: '.hbs',
+});
+
+const app = express();
+
+app.engine('.hbs', hbs.engine);
+app.set('view engine', '.hbs');
+
+app.use(express.urlencoded({ extended:true }));
+app.use('/static', express.static('static'))
+
+
+
+app.listen(3000, ()=>console.log('Server is listening on port 3000'));
