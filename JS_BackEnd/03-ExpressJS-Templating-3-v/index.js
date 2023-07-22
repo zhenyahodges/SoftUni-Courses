37277ef8840935cacf1c62837551faecf09d79f6
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-07-22 12:37:29 +0100

    Update index.js

--- JS_BackEnd/03-ExpressJS-Templating-3-v/index.js
+++ JS_BackEnd/03-ExpressJS-Templating-3-v/index.js
@@ -7,4 +7,38 @@ app.get('/', (req, res) => {
     res.send('Hello there');
 });
 
+app.route('/create')
+.get((req,res)=>{
+    res.send('what');
+})
+.post((req, res) => {
+    res.status(201).send('post request');
+});
+
+app.get('/catalog', (req, res) => {
+    res.send('Catalog');
+});
+
+app.get('/catalog/:productId', (req, res) => {
+    console.log(req.params.productId);
+    res.send('Product details');
+});
+
+app.get('/data', (req,res)=>{
+    res.json([
+        {
+            name:'Peter',
+            age:25
+        },
+        {
+            name:'John',
+            age: 35
+        }
+    ])
+})
+
+app.all('*', (req, res) => {
+    res.status(404).send('404 custom');
+});
+
 app.listen(3000);
