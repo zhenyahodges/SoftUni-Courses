f586705124fcdd0ef72f11d4c3289c39715ca162
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-07-22 14:06:59 +0100

    logger

--- JS_BackEnd/03-ExpressJS-Templating-3-v/createController.js
+++ JS_BackEnd/03-ExpressJS-Templating-3-v/createController.js
@@ -3,10 +3,19 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.send('<form method="post"><input name="name"><button>Send</button></form>');
+    res.send(
+        '<form method="post"><input name="name"><button>Send</button></form>'
+    );
 });
-router.post('/', (req, res) => {
-    res.redirect('/catalog');
-});
+router.post(
+    '/',
+    // (req, res, next) => {
+    //     console.log('handling post');
+    //     next();
+    // },
+    (req, res) => {
+        res.redirect('/catalog');
+    }
+);
 
 module.exports = router;
--- JS_BackEnd/03-ExpressJS-Templating-3-v/index.js
+++ JS_BackEnd/03-ExpressJS-Templating-3-v/index.js
@@ -1,30 +1,39 @@
 // require('express')().listen(3000);
 const express = require('express');
-const catalogController= require('./catalogController')
-const createController= require('./createController')
+const catalogController = require('./catalogController');
+const createController = require('./createController');
+const logger = require('./logger');
 
 const app = express();
 
 app.get('/', (req, res) => {
     // res.send('Hello there');
-    res.sendFile(__dirname+'/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.use('/create', createController)
-app.use('/catalog',catalogController)
+app.use(logger());
+app.use('/create', createController);
+app.use(
+    '/catalog',
+    // (req, res, next) => {
+    //     console.log(req.method, req.url);
+    //     next();
+    // },
+    catalogController
+);
 
-app.get('/data', (req,res)=>{
+app.get('/data', (req, res) => {
     res.json([
         {
-            name:'Peter',
-            age:25
+            name: 'Peter',
+            age: 25,
         },
         {
-            name:'John',
-            age: 35
-        }
-    ])
-})
+            name: 'John',
+            age: 35,
+        },
+    ]);
+});
 
 app.all('*', (req, res) => {
     res.status(404).send('404 custom');
