c7e914397775b9e5b1c0d156f548f7689c0dcaa2
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-07-22 14:58:32 +0100

    layout, partials

--- JS_BackEnd/03-ExpressJS-Templating-4-v/index.js
+++ JS_BackEnd/03-ExpressJS-Templating-4-v/index.js
@@ -2,8 +2,36 @@ const express = require('express');
 const hbr = require('express-handlebars');
 
 const app = express();
-hbr.create({ extname: '.hbs' });
+
+const handlebars = hbr.create({ extname: '.hbs' });
 app.engine('.hbs', handlebars.engine);
 app.set('view engine', '.hbs');
+
+app.get('/', (req,res)=>{
+    // res.locals.message='Hello'
+    // res.locals.response='blabla'
+    res.render('home',
+    {
+        username:'Maya',
+        title: 'Demo',
+        message:'Hi',
+        response: 'durabura',
+        product: {
+            name: 'Artichoke',
+            price: 2.00
+        },
+        contacts:[
+            {
+                name: 'Ivan',
+                email: 'ivan@abv.bg',
+            },
+            {
+                name: 'Mima',
+                email: 'mima@abv.bg',
+            }
+        ]
+    }
+    )
+})
 
 app.listen(3000);
