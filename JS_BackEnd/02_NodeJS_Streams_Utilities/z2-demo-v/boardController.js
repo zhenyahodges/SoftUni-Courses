e2a46631c239e37952bd6d8abeefb90af1071cc4
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-07-22 10:15:42 +0100

    updates

--- JS_BackEnd/02_NodeJS_Streams_Utilities/z2-demo-v/boardController.js
+++ JS_BackEnd/02_NodeJS_Streams_Utilities/z2-demo-v/boardController.js
@@ -1,27 +1,40 @@
-const fs = require('fs');
+const { createReadStream, promises: fs } = require('fs');
 
-function homePage(req, res) {
+async function homePage(req, res) {
     res.writeHead(200, {
         'Content-Type': 'text/html',
     });
 
-    fs.readFile('./static/index.html', (err, layout) => {
-        res.write(layout.toString().replace('<%%imageboard%%>', '<p>Dynamic</p>'));
-        res.end();
-    });
+    const layout = await fs.readFile('./static/index.html');
+
+    res.write(
+        layout.toString().replace('<%%imageboard%%>', await getImageList())
+    );
+    res.end();
 }
 
 function sendFile(req, res) {
     const path = req.url;
+    
     if (path == '/style.css') {
         res.writeHead(200, {
             'Content-Type': 'text/css',
         });
-        fs.createReadStream('./static/style.css').pipe(res);
+        createReadStream('./static/style.css').pipe(res);
     } else {
         const fileName = '.' + path;
-        fs.createReadStream(fileName).pipe(res);
+        createReadStream(fileName).pipe(res);
     }
+}
+
+async function getImageList() {
+    const files = await fs.readdir('./img');
+
+    return `<ul>${files.map(imageCard)}</ul>`;
+}
+
+function imageCard(imgName) {
+    return `<li><img src="./img/${imgName}"></li>`;
 }
 
 module.exports = {
--- JS_BackEnd/02_NodeJS_Streams_Utilities/z2-demo-v/main.js
+++ JS_BackEnd/02_NodeJS_Streams_Utilities/z2-demo-v/main.js
@@ -3,15 +3,16 @@ const { createImage } = require('./createController');
 
 function handleRequest(req, res) {
     let handler;
-    let path = req.url;
+    const path = req.url;
 
     if (req.method == 'GET') {
         if (path == '/style.css' || path.slice(0, 4) == '/img') {
             handler = sendFile;
-
         } else if (path == '/') {
             handler = homePage;
-        } 
+        }
+    } else if (req.method == 'POST') {
+        handler = createImage;
     }
 
     if (typeof handler == 'function') {
