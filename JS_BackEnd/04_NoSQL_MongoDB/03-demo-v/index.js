79c5a8ca0afe870fb910d59ea213a76564da43fe
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-07-23 11:06:14 +0100

    Update index.js

--- JS_BackEnd/04_NoSQL_MongoDB/03-demo-v/index.js
+++ JS_BackEnd/04_NoSQL_MongoDB/03-demo-v/index.js
@@ -1,8 +1,20 @@
-const mongodb=require('mongodb');
+const mongodb = require('mongodb');
 
-const connectionString='mongodb://127.0.0.1:27017';
-// 'mongodb://localhost:27017';
+const connectionString = 'mongodb://127.0.0.1:27017' 
+    //  'mongodb://localhost:27017';
+start();
 
-const connection = new mongodb.MongoClient(connectionString,{
-    useUnifiedTopology: true
-})
+async function start() {
+    const connection = new mongodb.MongoClient(connectionString, {
+        useUnifiedTopology: true,
+    });
+
+    await connection.connect();
+
+    const db = connection.db('user-list');
+    const collection = db.collection('users');
+    const query = collection.find({});
+    const data = await query.toArray();
+
+    console.log(data);
+}
