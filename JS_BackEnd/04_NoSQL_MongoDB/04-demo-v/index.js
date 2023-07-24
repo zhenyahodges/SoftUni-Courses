d34a45597d1520324f42b1c0d3c97ae32a5fcf5b
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-07-24 10:59:27 +0100

    db connect

--- JS_BackEnd/04_NoSQL_MongoDB/04-demo-v/index.js
+++ JS_BackEnd/04_NoSQL_MongoDB/04-demo-v/index.js
@@ -2,13 +2,25 @@ const express = require('express');
 const hbs = require('express-handlebars').create({
     extname: '.hbs',
 });
+const mongoose=require('mongoose');
+const connStr= 'mongodb://127.0.0.1:27017/testdb2';
 
-const app=express()
-app.engine('.hbs', hbs.engine)
-app.set('view engine', '.hbs')
+start()
 
-app.get('/', (req,res)=>{
-    res.render('home')
-})
+async function start() {
+    const app = express();
+    app.engine('.hbs', hbs.engine);
+    app.set('view engine', '.hbs');
 
-app.listen(3000)
+    app.get('/', (req, res) => {
+        res.render('home');
+    });
+
+   await mongoose.connect(connStr, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    })
+    console.log('DB ready');
+
+    app.listen(3000, () => console.log('Server listening on port 3000'));
+}
