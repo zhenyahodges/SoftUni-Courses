a3e6012400a4a9cccc408d5ff6ad0dbf0515fb7a
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-07-30 09:42:54 +0100

    updates

--- JS_BackEnd/05-Sessions-and-Authentication/demo1-v-sess-auth/auth.js
+++ JS_BackEnd/05-Sessions-and-Authentication/demo1-v-sess-auth/auth.js
@@ -1,35 +1,50 @@
-const app = require('express')();
+const express = require('express');
 const session = require('express-session');
 
-app.use(session({
-    cookie: { secure: false },
-    resave: false,
-    saveUninitialized: true,
-    secret: 'my secret',
-}));
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+    session({
+        cookie: { secure: false },
+        resave: false,
+        saveUninitialized: true,
+        secret: 'my secret',
+    })
+);
 
 app.get('/', (req, res) => {
-    console.log(req.session);
-    if(req.session.user){
+    console.log(`User:` + (req.session.user || `guest`));
+    if (req.session.user) {
         res.send(`<p>Hello, ${req.session.user}!</p>`);
-
-    }else{
-
+    } else {
         res.send(`<p>Hello,guest, please <a href="/login">login here</a></p>`);
     }
 });
 
 app.get('/login', (req, res) => {
-    res.send(`<form action="login" method="post">
+    res.send(`<form action="/login" method="post">
     <label>Username: <input type="text" name="username"></label>
     <label>Password: <input type="password" name="password"></label>
     <input type="submit" value="Login">
-    </form>`)
+    </form>`);
 });
 
-app.post('/login',(req, res) => {
-    console.log(req.session);
-    req.session.user='Peter'
-    res.redirect('/');
-})
-app.listen(3000)
+const users = {
+    Peter: '123',
+    John: '123',
+};
+
+app.post('/login', (req, res) => {
+    console.log('login attempt');
+    // console.log(req.session);
+    const username = req.body.username;
+    if (users[username] != undefined && users[username] == req.body.password) {
+        req.session.user = username;
+        res.redirect('/');
+    } else {
+        res.status(401).send('Incorrect username or password');
+    }
+});
+app.listen(3000);
