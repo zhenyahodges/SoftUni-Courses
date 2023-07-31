db54300886b2f59a13f44bcdb9073a64a0a12a98
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-07-31 10:39:51 +0100

    portect edit route

--- JS_BackEnd/05-Sessions-and-Authentication/workshop-v/controllers/roomController.js
+++ JS_BackEnd/05-Sessions-and-Authentication/workshop-v/controllers/roomController.js
@@ -6,6 +6,10 @@ roomController.get('/:id/edit', async (req, res) => {
     const roomId = req.params.id;
     const room = await getById(roomId);
 
+    if(!req.user || room.owner!=req.user._id){
+        return res.redirect('/auth/login');
+    }
+
     res.render('edit', {
         title: 'Edit Accomodation',
         room,
@@ -14,6 +18,12 @@ roomController.get('/:id/edit', async (req, res) => {
 
 roomController.post('/:id/edit', async (req, res) => {
     const roomId = req.params.id;
+    const room = await getById(roomId);
+
+    if(!req.user || room.owner!=req.user._id){
+        return res.redirect('/auth/login');
+    }
+
     try {
         const result = await update(roomId, req.body);
         res.redirect('/catalog/' + result._id);
