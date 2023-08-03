e93ac5370ab03bc77c3e8b892dc37ef1700c0dab
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-08-03 11:08:28 +0100

    bookinUni-v-exam-prep

---
+++ JS_BackEnd/Exam-Prep/V-Exam-Prep/Booking-exam-prep/BookingUni/middlewares/session.js
@@ -0,0 +1,21 @@
+const { verifyToken } = require('../services/userService');
+
+module.exports = () => (req, res, next) => {
+    const token = req.cookies.token;
+
+    if (token) {
+        // console.log(token);
+        try {
+            const userData = verifyToken(token);
+            // console.log('read success', userData);
+            req.user = userData;
+        } catch (error) {
+            // console.log('invalid token');
+            res.clearCookie('token');
+            res.redirect('/auth/login');
+            return;
+        }
+    }
+
+    next();
+};
