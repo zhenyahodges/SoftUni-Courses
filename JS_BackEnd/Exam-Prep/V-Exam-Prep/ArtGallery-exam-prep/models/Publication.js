76f66d89f33c6f7877271c101786caa8ebd3b809
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-08-06 14:20:48 +0100

    Update Publication.js

--- JS_BackEnd/Exam-Prep/V-Exam-Prep/ArtGallery-exam-prep/models/Publication.js
+++ JS_BackEnd/Exam-Prep/V-Exam-Prep/ArtGallery-exam-prep/models/Publication.js
@@ -1,23 +1,50 @@
-const { Schema, model,Types } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 
-const publicationSchema=new Schema({
-    title:{
+const URL_PATTERN=/^https?:\/\/.+$/i;
+
+const publicationSchema = new Schema({
+    title: {
         type: String,
         required: true,
+        minlength: [6, 'Title must be at least 6 characters long'],
     },
-    technique:{
+    technique: {
+        type: String,
+        required: true,
+        maxlength: [15, 'Title must be at most 15 characters long'],
+    },
+    artPicture:{
         type:String,
         required: true,
+        validate: {
+            validator: (value)=>{URL_PATTERN.test(value)},
+            message: 'ImageUrl not valid'
+        }
     },
-    certificate:{
-        enum:['YES', 'NO'],
+    certificate: {
+        enum: ['YES', 'NO'],
         required: true,
     },
-    author:{
-        type:
-    }
-})
+    author: {
+        type: Types.ObjectId,
+        ref: 'User',
+    },
+    shared: {
+        type: [Types.ObjectId],
+        ref: 'User',
+        default: [],
+    },
+});
 
+publicationSchema.index(
+    { title: 1 },
+    {
+        collation: {
+            locale: 'en',
+            strength: 2,
+        },
+    }
+);
 
 const Publication = model('Publication', publicationSchema);
 
