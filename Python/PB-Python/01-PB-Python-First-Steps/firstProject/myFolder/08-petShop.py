e624e20449dc2b2fe0617611360a681c19a8be64
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2024-01-18 19:34:15 +0000

    Update 08-petShop.py

---
+++ Python/PB-Python/01-PB-Python-First-Steps/firstProject/myFolder/08-petShop.py
@@ -0,0 +1,7 @@
+dogFoodQuantity = int(input())
+catFoodQuantity = int(input())
+dogFoodPrice = 2.50
+catFoodPrice = 4
+totalSum = (dogFoodPrice * dogFoodQuantity) + (catFoodPrice * catFoodQuantity)
+
+print(f'{totalSum} lv.')
