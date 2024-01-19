58bab3069e929009a13ff8a6c8d4d40aad71a502
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2024-01-19 17:52:18 +0000

    Create 01-Sum-Seconds.py

---
+++ Python/PB-Python/PB-Python-Lab-and-Exercises/02-PB-Python-Conditional-Statements/02-PB-Python-Conditional-Statements-exercises/01-Sum-Seconds.py
@@ -0,0 +1,13 @@
+time_sec_1 = int(input())
+time_sec_2 = int(input())
+time_sec_3 = int(input())
+
+total = time_sec_1 + time_sec_2 + time_sec_3
+
+minutes = total // 60
+seconds = total % 60
+
+if seconds<10:
+    print(f'{minutes}:0{seconds}')
+else:
+    print(f'{minutes}:{seconds}')
