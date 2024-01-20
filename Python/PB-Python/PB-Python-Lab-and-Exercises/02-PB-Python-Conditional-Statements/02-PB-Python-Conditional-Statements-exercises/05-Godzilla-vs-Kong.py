07c2780f38acce6a20691cebda7bb4879a75ca09
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2024-01-20 23:30:01 +0000

    Create 05-Godzilla-vs-Kong.py

---
+++ Python/PB-Python/PB-Python-Lab-and-Exercises/02-PB-Python-Conditional-Statements/02-PB-Python-Conditional-Statements-exercises/05-Godzilla-vs-Kong.py
@@ -0,0 +1,17 @@
+budget = float(input())
+extras = int(input())
+clothing_cost_per_xtra = float(input())
+
+decor = budget * 0.1
+
+if extras > 150:
+    clothing_cost_per_xtra = clothing_cost_per_xtra * 0.90
+
+total=extras*clothing_cost_per_xtra+decor
+
+if total >budget:
+    print("Not enough money!")
+    print(f'Wingard needs {total-budget:.2f} leva more.')
+elif total<=budget:
+    print("Action!")
+    print(f"Wingard starts filming with {budget-total:.2f} leva left.")
