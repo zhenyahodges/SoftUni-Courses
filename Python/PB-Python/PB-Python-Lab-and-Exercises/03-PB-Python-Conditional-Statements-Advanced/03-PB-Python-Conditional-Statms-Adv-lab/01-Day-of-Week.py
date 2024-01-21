70b81c140486968ac1945b8f8fae0d34649d1e76
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2024-01-21 14:13:39 +0000

    Create 01-Day-of-Week.py

---
+++ Python/PB-Python/PB-Python-Lab-and-Exercises/03-PB-Python-Conditional-Statements-Advanced/03-PB-Python-Conditional-Statms-Adv-lab/01-Day-of-Week.py
@@ -0,0 +1,22 @@
+num = int(input())
+result = ''
+
+if num == 1:
+    result = 'Monday'
+elif num == 2:
+    result = 'Tuesday'
+elif num == 3:
+    result = 'Wednesday'
+elif num == 4:
+    result = 'Thursday'
+elif num == 5:
+    result = 'Friday'
+elif num == 6:
+    result = 'Saturday'
+elif num == 7:
+    result = 'Sunday'
+else:
+    result = 'Error'
+
+print(result)
+
