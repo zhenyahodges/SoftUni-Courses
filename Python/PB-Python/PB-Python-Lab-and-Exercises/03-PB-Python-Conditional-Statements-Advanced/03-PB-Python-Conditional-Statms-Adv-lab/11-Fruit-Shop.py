ad0ce2c0b7ffce4a4c1c916ee9e3156004c698cf
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2024-01-22 14:10:32 +0000

    Update 11-Fruit-Shop.py

---
+++ Python/PB-Python/PB-Python-Lab-and-Exercises/03-PB-Python-Conditional-Statements-Advanced/03-PB-Python-Conditional-Statms-Adv-lab/11-Fruit-Shop.py
@@ -0,0 +1,45 @@
+fruit = str(input())
+day = str(input())
+quantity = float(input())
+price = 0
+
+if day in ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']:
+    if fruit == 'banana':
+        price = 2.50
+    elif fruit == 'apple':
+        price = 1.20
+    elif fruit == 'orange':
+        price = 0.85
+    elif fruit == 'grapefruit':
+        price = 1.45
+    elif fruit == 'kiwi':
+        price = 2.70
+    elif fruit == 'pineapple':
+        price = 5.50
+    elif fruit == 'grapes':
+        price = 3.85
+    else:
+        print('error')
+elif day in ['Saturday', 'Sunday']:
+    if fruit == 'banana':
+        price = 2.70
+    elif fruit == 'apple':
+        price = 1.25
+    elif fruit == 'orange':
+        price = 0.90
+    elif fruit == 'grapefruit':
+        price = 1.60
+    elif fruit == 'kiwi':
+        price = 3.00
+    elif fruit == 'pineapple':
+        price = 5.60
+    elif fruit == 'grapes':
+        price = 4.20
+    else:
+        print('error')
+else:
+    print('error')
+
+if price > 0:
+    total = price * quantity
+    print(f'{total:.2f}')
