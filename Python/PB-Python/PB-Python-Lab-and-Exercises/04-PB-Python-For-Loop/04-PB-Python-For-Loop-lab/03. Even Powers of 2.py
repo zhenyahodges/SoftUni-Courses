0c6331a85c58c96a5f0fc11f80a4cfcf969fb9bd
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2024-01-28 17:18:07 +0000

    lab

---
+++ Python/PB-Python/PB-Python-Lab-and-Exercises/04-PB-Python-For-Loop/04-PB-Python-For-Loop-lab/03. Even Powers of 2.py
@@ -0,0 +1,7 @@
+n = int(input())
+
+for number in range(n + 1):
+    if number % 2 == 0:
+        print(2 ** number)
+
+
---
+++ Python/PB-Python/PB-Python-Lab-and-Exercises/04-PB-Python-For-Loop/04-PB-Python-For-Loop-lab/demo-print-zero.py
@@ -0,0 +1,9 @@
+for number in range(10, -1, -2):
+    print(number)
+
+for number in range(10,0,-2):
+    print(number)
+
+for number in range(0,11, -2):
+    print(number)
+
