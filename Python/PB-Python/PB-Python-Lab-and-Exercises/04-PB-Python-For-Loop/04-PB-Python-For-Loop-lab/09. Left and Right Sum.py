ae759b11f9dd50d394952ab48f8f229de8717131
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2024-01-29 22:22:14 +0000

    Update 09. Left and Right Sum.py

--- Python/PB-Python/PB-Python-Lab-and-Exercises/04-PB-Python-For-Loop/04-PB-Python-For-Loop-lab/09. Left and Right Sum.py
+++ Python/PB-Python/PB-Python-Lab-and-Exercises/04-PB-Python-For-Loop/04-PB-Python-For-Loop-lab/09. Left and Right Sum.py
@@ -13,3 +13,20 @@ if left_sum == right_sum:
 else:
     diff = abs(left_sum - right_sum)
     print(f'No, diff = {diff}')
+
+# left_sum = 0
+# right_sum = 0
+#
+# n = int(input())
+# for i in range(1, 2 * n + 1):
+#     num = int(input())
+#     if i <= n:
+#         left_sum += num
+#     else:
+#         right_sum += num
+#
+# if left_sum == right_sum:
+#     print(f"Yes, sum = {left_sum}")
+# else:
+#     diff = left_sum - right_sum
+#     print(f"No, diff = {abs(diff)}")
