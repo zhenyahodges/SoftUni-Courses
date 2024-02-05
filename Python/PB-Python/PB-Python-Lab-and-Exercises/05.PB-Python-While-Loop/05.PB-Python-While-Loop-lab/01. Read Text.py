b344e8542b36fe91e51a2448da792a52008c04a2
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2024-02-05 14:53:48 +0000

    lab

---
+++ Python/PB-Python/PB-Python-Lab-and-Exercises/05.PB-Python-While-Loop/05.PB-Python-While-Loop-lab/01. Read Text.py
@@ -0,0 +1,5 @@
+text = str(input())
+
+while text != 'Stop':
+    print(text)
+    text = str(input())
---
+++ Python/PB-Python/PB-Python-Lab-and-Exercises/05.PB-Python-While-Loop/05.PB-Python-While-Loop-lab/02. Password.py
@@ -0,0 +1,8 @@
+name = str(input())
+pass_to_match = input()
+checkPass = input()
+
+while checkPass != pass_to_match:
+    checkPass = input()
+
+print(f'Welcome {name}!')
