54d67da7036cfc8b8b5a0d0f4f6f72933f2bc439
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2024-02-05 18:34:17 +0000

    Update 01. Read Text.py

--- Python/PB-Python/PB-Python-Lab-and-Exercises/05.PB-Python-While-Loop/05.PB-Python-While-Loop-lab/01. Read Text.py
+++ Python/PB-Python/PB-Python-Lab-and-Exercises/05.PB-Python-While-Loop/05.PB-Python-While-Loop-lab/01. Read Text.py
@@ -3,3 +3,12 @@ text = str(input())
 while text != 'Stop':
     print(text)
     text = str(input())
+
+
+# while True:
+#     word = input()
+#
+#     if word == 'Stop':
+#         break
+#
+#     print(word)
