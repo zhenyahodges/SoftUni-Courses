3a241962eb082f65fea0b4f04774b2ac37cfef5e
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2024-02-10 12:17:04 +0000

    Update 05. Coins.py

--- Python/PB-Python/PB-Python-Lab-and-Exercises/05.PB-Python-While-Loop/05.PB-Python-While-Loop-exercise/05. Coins.py
+++ Python/PB-Python/PB-Python-Lab-and-Exercises/05.PB-Python-While-Loop/05.PB-Python-While-Loop-exercise/05. Coins.py
@@ -3,30 +3,23 @@ coins = 0
 
 while change > 0:
     if change >= 2:
-        coins += 1
         change -= 2
     elif change >= 1:
-        coins += 1
         change -= 1
     elif change >= 0.50:
-        coins += 1
         change -= 0.50
     elif change >= 0.20:
-        coins += 1
         change -= 0.20
     elif change >= 0.10:
-        coins += 1
         change -= 0.10
     elif change >= 0.05:
-        coins += 1
         change -= 0.05
     elif change >= 0.02:
-        coins += 1
         change -= 0.02
     elif change >= 0.01:
-        coins += 1
         change -= 0.01
 
+    coins += 1
     change = round(change, 2)
 
 print(coins)
