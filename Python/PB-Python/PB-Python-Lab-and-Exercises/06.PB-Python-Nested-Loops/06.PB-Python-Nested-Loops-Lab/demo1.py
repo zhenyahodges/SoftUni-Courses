f1aa131642e8d9931e9bdccb0ba53ea3966091a7
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2024-02-15 22:41:35 +0000

    Update demo1.py

--- Python/PB-Python/PB-Python-Lab-and-Exercises/06.PB-Python-Nested-Loops/06.PB-Python-Nested-Loops-Lab/demo1.py
+++ Python/PB-Python/PB-Python-Lab-and-Exercises/06.PB-Python-Nested-Loops/06.PB-Python-Nested-Loops-Lab/demo1.py
@@ -2,3 +2,12 @@ for hour in range(24):
     for minute in range(60):
         for second in range(60):
             print(f'{hour}:{minute:02d}:{second:02d}')
+
+            if hour == 1:
+                break
+
+        if hour == 1:
+            break
+
+    if hour == 1:
+        break
