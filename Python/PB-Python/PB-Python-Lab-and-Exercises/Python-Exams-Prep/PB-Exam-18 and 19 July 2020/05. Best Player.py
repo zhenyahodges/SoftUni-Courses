2545a378f90f259086f8691d3ea711073642aaa0
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2024-02-15 07:49:37 +0000

    exam prep

--- Python/PB-Python/PB-Python-Lab-and-Exercises/Python-Exams-Prep/PB-Exam-18 and 19 July 2020/05. Best Player.py
+++ Python/PB-Python/PB-Python-Lab-and-Exercises/Python-Exams-Prep/PB-Exam-18 and 19 July 2020/05. Best Player.py
@@ -1,3 +1,29 @@
+import sys
 
+command = input()
+max_num = -sys.maxsize
+top_score_name = ''
+has_hat_trick = False
 
+while command != 'END':
+    name = command
+    score = int(input())
 
+    if score > max_num:
+        max_num = score
+        top_score_name = name
+
+        if score >= 3 and top_score_name == name:
+            has_hat_trick = True
+
+    if score >= 10:
+        break
+
+    command = input()
+
+print(f"{top_score_name} is the best player!")
+
+if has_hat_trick:
+    print(f"He has scored {max_num} goals and made a hat-trick !!!")
+else:
+    print(f"He has scored {max_num} goals.")
