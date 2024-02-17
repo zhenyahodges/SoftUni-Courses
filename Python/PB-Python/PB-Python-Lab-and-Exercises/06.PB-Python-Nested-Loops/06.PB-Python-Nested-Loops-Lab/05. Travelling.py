40397dd1361f0d2464eec2dca22d163f04181a94
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2024-02-17 09:59:14 +0000

    loops

--- Python/PB-Python/PB-Python-Lab-and-Exercises/06.PB-Python-Nested-Loops/06.PB-Python-Nested-Loops-Lab/05. Travelling.py
+++ Python/PB-Python/PB-Python-Lab-and-Exercises/06.PB-Python-Nested-Loops/06.PB-Python-Nested-Loops-Lab/05. Travelling.py
@@ -1,1 +1,15 @@
-destination=str(input())
+destination = str(input())
+is_going = False
+
+while destination != 'End':
+    cost = float(input())
+
+    while cost > 0:
+        saved = float(input())
+        cost -= saved
+        is_going = False
+    else:
+        is_going = True
+        print(f"Going to {destination}!")
+
+    destination = str(input())
