1bb37f8b8cd74a86214b8d7e81a4a7fe99278df9
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2024-02-17 10:31:37 +0000

    alt solution

--- Python/PB-Python/PB-Python-Lab-and-Exercises/06.PB-Python-Nested-Loops/06.PB-Python-Nested-Loops-Lab/06. Building.py
+++ Python/PB-Python/PB-Python-Lab-and-Exercises/06.PB-Python-Nested-Loops/06.PB-Python-Nested-Loops-Lab/06. Building.py
@@ -1,16 +1,29 @@
 floors = int(input())
 rooms_per_floor = int(input())
 flat_name = ''
+sign = ''
 
-for floor in range(floors, 0, -1):
+# for floor in range(floors, 0, -1):
+#     for room in range(rooms_per_floor):
+#         if floor == floors:
+#             flat_name = f'L{floor}{room}'
+#         else:
+#             if floor % 2 == 0:
+#                 flat_name = f'O{floor}{room}'
+#             else:
+#                 flat_name = f'A{floor}{room}'
+#
+#         print(flat_name, end=' ')
+#     print()
+
+for floor in reversed(range(1, floors + 1)):
+    sign = 'A' if floor % 2 else 'O'
+
+    if floor == floors:
+        sign = 'L'
+
     for room in range(rooms_per_floor):
-        if floor == floors:
-            flat_name = f'L{floor}{room}'
-        else:
-            if floor % 2 == 0:
-                flat_name = f'O{floor}{room}'
-            else:
-                flat_name = f'A{floor}{room}'
-
+        flat_name = f'{sign}{floor}{room}'
         print(flat_name, end=' ')
+
     print()
