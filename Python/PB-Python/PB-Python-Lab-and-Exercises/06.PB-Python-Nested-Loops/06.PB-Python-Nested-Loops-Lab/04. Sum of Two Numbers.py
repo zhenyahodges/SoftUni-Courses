e358eab6b01b56ade041ac315943b0bd1c7c574c
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2024-02-17 09:41:42 +0000

    exercises

---
+++ Python/PB-Python/PB-Python-Lab-and-Exercises/06.PB-Python-Nested-Loops/06.PB-Python-Nested-Loops-Lab/04. Sum of Two Numbers.py
@@ -0,0 +1,18 @@
+start = int(input())
+end = int(input())
+magic_num = int(input())
+count = 0
+is_found = False
+
+for first_num in range(start, end + 1):
+    for second_num in range(start, end + 1):
+        count += 1
+        if first_num + second_num == magic_num:
+            is_found = True
+            print(f"Combination N:{count} ({first_num} + {second_num} = {magic_num})")
+            break
+    if is_found:
+        break
+
+if not is_found:
+    print(f"{count} combinations - neither equals {magic_num}")
