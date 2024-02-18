2949d8c38ac6f935a4490c670ceae1d3512ae28a
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2024-02-18 11:08:09 +0000

    Update 05. Special Numbers.py

---
+++ Python/PB-Python/PB-Python-Lab-and-Exercises/06.PB-Python-Nested-Loops/06.PB-Python-Nested-Loops-exercises/05. Special Numbers.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+# for _ in range(n):
+for x in range(1_111, 10_000):
+    num_to_str = str(x)
+
+    is_special = True
+    for d in num_to_str:
+        digit = int(d)
+
+        if digit == 0:
+            is_special = False
+            break
+
+        if n % digit != 0:
+            is_special = False
+            break
+
+    if is_special:
+        print(num_to_str, end=' ')
