4be67983342a01c989440debe06d27a31b13c85e
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2024-02-18 07:01:27 +0000

    Update 01. Number Pyramid.py

---
+++ Python/PB-Python/PB-Python-Lab-and-Exercises/06.PB-Python-Nested-Loops/06.PB-Python-Nested-Loops-exercises/01. Number Pyramid.py
@@ -0,0 +1,17 @@
+n = int(input())
+current = 1
+is_curr_bigger_than_n = False
+
+for row in range(1, n + 1):
+    for col in range(1, row + 1):
+
+        if current > n:
+            is_curr_bigger_than_n = True
+            break
+
+        print(str(current) + ' ', end='')
+        current += 1
+
+    if is_curr_bigger_than_n:
+        break
+    print()
